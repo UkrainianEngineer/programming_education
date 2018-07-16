348e8badf3ac2fb1a7382e71175f0b49ac370837
Victoria Stasiv <Viktoria99y@icloud.com>
2018-07-16 07:28:56 +0300

    exercise

--- victoria_stasiv/exercise01.py
+++ victoria_stasiv/exercise01.py
@@ -1,9 +1,14 @@
 data = ["one", "1", "two", "2", "ten", "10", "15", "-2", "some long text goes here", "16.9", "&"]
 def get_max_value(data):
+  all = list()
   l = list()
   for element in data:
     m = element.replace(" ", "")
-    if not m.isalpha() and not m=='&':
-     l.append(float(element))
-  return int(max(l))
-print(get_max_value(data))
+    k = m.split('.')
+    lst =[k[0]]
+    for l in k:
+     if m.lstrip('-').isdigit() or l.isdigit():
+        all.append(float(m));
+  print(int(max(all)))
+
+get_max_value(data)
