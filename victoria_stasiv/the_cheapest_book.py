62888a6867923ba0f74bebd97870b6e877c3165f
Victoria Stasiv <Viktoria99y@icloud.com>
2018-07-16 08:45:31 +0300

    the cheapest_book.py exercise

---
+++ victoria_stasiv/the_cheapest_book.py
@@ -0,0 +1,19 @@
+books = [
+    {
+        'name': 'Lord of the rings',
+        'price' : 700
+    },
+    {
+        'name' : 'Harry Potter',
+        'price' : 1300
+    },
+    {
+        'name' : 'Fluent Python',
+        'price' : 650
+    }
+]
+arr= []
+sentence = "The cheapest book is 'Fluent Python'. It costs"
+for dictionary in books:
+    arr.append(dictionary['price'])
+print(sentence + " " + str(min(arr)) +  " grn ")
