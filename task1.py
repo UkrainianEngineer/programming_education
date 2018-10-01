89941b6147469fd292502c01af1f93c2c4322965
Olya Petryshyn <olya.petryshyn@gmail.com>
2018-10-02 00:36:04 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,9 +1,8 @@
-# task 1
-num = '12345'
-num1 = '611091234512'
+first_number_to_count_digits = '12345'
+second_number_to_count_digits = '611091234512'
 def add_integers(str_number):
      result = 0
      for i in str_number:
           result += int(i)
      return result
-print('First result: ' + str(add_integers(num)) + '\nSecond result: ' + str(add_integers(num1)))
+print('First result: ' + str(add_integers(first_number_to_count_digits)) + '\nSecond result: ' + str(add_integers(second_number_to_count_digits)))
