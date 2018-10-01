3362e48b0b6f252050054b644bb0467bfa4a0e38
Oleksiy Tiulpin <oleksiy@btech.com.ua>
2018-10-01 22:17:28 +0300

    Some changes

--- oleksijt/Task1.py
+++ oleksijt/Task1.py
@@ -1,24 +1,23 @@
-##Calculate sum of all digits of a numer set in string.
-##
-##Example:
-##
-##num = ‘12345’
-##
-##Your script should calculate it like: 1+2+3+4+5 = 15
-##
-##num = ‘611091234512’
-##result = 6+1+1+0+9+1+2+3+4+6+1+2 = 36
+"""Calculate sum of all digits of a numer set in string.
+
+Example:
+
+num = ‘12345’
+
+Your script should calculate it like: 1+2+3+4+5 = 15
+
+num = ‘611091234512’
+result = 6+1+1+0+9+1+2+3+4+6+1+2 = 36
+"""
+
 
 num = '611091234512'
-
-##set sum variable 
+# Set sum variable 
 result = 0
-
-##fill text string and calculate sum 
+# Fill text string and calculate sum 
 for digit in num:
     result += int(digit)
-    
-##print result
-print (result)
+# Print result
+print(result)
     
 
--- oleksijt/Task2.py
+++ oleksijt/Task2.py
@@ -1,23 +1,26 @@
-##2. Find the cheapest book
-##
-##books = [
-##    {
-##        ‘name’: ‘Lord of the rings’,
-##        ‘price’: 700
-##    },
-##    {
-##        ‘name’: ‘Harry Potter’,
-##        ‘price’: 1300
-##    },
-##    {
-##        ‘name’: ‘Fluent Python’,
-##        ‘price’: 650
-##    }
-##]
-##
-##Your script should return: `The cheapest book is ‘Fluent Python’. It costs 650 grn.`
+"""2. Find the cheapest book
 
-#fill dict
+books = [
+    {
+        ‘name’: ‘Lord of the rings’,
+        ‘price’: 700
+    },
+    {
+        ‘name’: ‘Harry Potter’,
+        ‘price’: 1300
+    },
+    {
+        ‘name’: ‘Fluent Python’,
+        ‘price’: 650
+    }
+]
+
+Your script should return:
+    `The cheapest book is ‘Fluent Python’. It costs 650 grn.`
+"""
+
+
+# Fill dict
 books = [{
         'name': 'Lord of the rings',
         'price': 700
@@ -31,13 +34,15 @@ books = [{
         'price': 650
     }]
 
-#set first book as cheapest
-cheapest_book = books[0]
-
-#look for cheapest bookin books
-for book in books:
-    if cheapest_book['price'] > book['price']:
-        cheapest_book = book
-
-#print result       
-print ('The cheapest book is ‘' + cheapest_book['name'] +'’. It costs ' + str(cheapest_book['price']) + ' grn.')
+try:
+    # Set first book as cheapest
+    cheapest_book = books[0]
+    # Look for cheapest bookin books
+    for book in books:
+        if cheapest_book['price'] > book['price']:
+            cheapest_book = book
+    # Print result       
+    print('The cheapest book is ‘' + cheapest_book['name']
+          + '’. It costs ' + str(cheapest_book['price']) + ' grn.')
+except:
+    print('No books found')
