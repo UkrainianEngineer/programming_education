e51653e4c189de61d81cc82220fa0af232a2b970
Oleksiy Tiulpin <oleksiy@btech.com.ua>
2018-10-02 20:16:12 +0300

    Task1 improved

--- oleksijt/Task1.py
+++ oleksijt/Task1.py
@@ -12,11 +12,8 @@ result = 6+1+1+0+9+1+2+3+4+6+1+2 = 36
 
 
 num = '611091234512'
-# Set sum variable 
-result = 0
-# Fill text string and calculate sum 
-for digit in num:
-    result += int(digit)
+# Calculate result 
+result = sum(int(digit) for digit in num)
 # Print result
 print(result)
     
