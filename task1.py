789942b51cc846173be4f9e7f51b87e0510d934c
Olya Petryshyn <olya.petryshyn@gmail.com>
2018-10-02 17:56:13 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,8 +1,8 @@
 first_number_to_count_digits = '12345'
 second_number_to_count_digits = '611091234512'
-def count_digits(str_number):
+def digit_sum(str_number):
      result = 0
      for element in str_number:
           result += int(element)
      return result
-print(f"First result: {count_digits(first_number_to_count_digits)} \nSecond result: {count_digits((second_number_to_count_digits))}"
+print(f"First result: {digit_sum(first_number_to_count_digits)} \nSecond result: {digit_sum((second_number_to_count_digits))}"
