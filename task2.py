9b3e61e99bcb9e79398478e87b00878c98a6851c
Olya Petryshyn <olya.petryshyn@gmail.com>
2018-10-02 14:18:54 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,5 +1,3 @@
-#task 2
-#from numpy import *
 books = [{
         'name': 'Lord of the rings',
         'price': 700
@@ -12,10 +10,5 @@ books = [{
         'name': 'Fluent Python',
         'price': 650
     }]
-cheapest_book_price = 0
-cheapest_book_name = ''
-for book in books:
-    if cheapest_book_price == 0 or cheapest_book_price > book['price']:
-        cheapest_book_price = book['price']
-        cheapest_book_name = book['name']
-print('The cheapest book is: ' + cheapest_book_name + '. It costs ' + str(cheapest_book_price) + ' grn.')
+cheapest_book = min(books, key=lambda x: x['price'])
+print(f'The cheapest book is:  {cheapest_book["name"]}. It costs: {cheapest_book["price"]} grn.')
