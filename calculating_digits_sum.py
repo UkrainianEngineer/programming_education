4571a85f5fde068333920453c0baf016498319ae
Olya Petryshyn <olya.petryshyn@gmail.com>
2018-10-03 15:32:47 +0300

    added docstring and changed the name of the file

--- calculating_digits_sum.py
+++ calculating_digits_sum.py
@@ -1,3 +1,6 @@
+"""
+In this file I am calculating the sm of digits in a string number.
+"""
 first_number_to_count_digits = '12345'
 second_number_to_count_digits = '611091234512'
 print(f'First result: {sum(int(digit) for digit in first_number_to_count_digits)}')
