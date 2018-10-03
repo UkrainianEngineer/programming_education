8fc24c4528c6fd3e9313a077c181fb5d83644656
Sasha1152 <saxin@ukr.net>
2018-10-03 14:23:15 +0300

    fixed 'calculate_digits' file

--- Oleksandr_Shmigelskiy/task01_calc_digits.py
+++ Oleksandr_Shmigelskiy/task01_calc_digits.py
@@ -1,31 +1,26 @@
-"""
-This function calculate sum of all digits of a number.
-A number can be represented in integer or string type.
-Value of the letters or any blank symbol equated to zero.
-
-Example:
-num = '12345'
-the function calculate it like: 1+2+3+4+5 = 15
-num = 'a5sd45g6'
-result = 5+4+5+6 = 20
-"""
-
 
 def calculate_digits(num):
-    if type(num) is int:
-        num = str(num)
+    """
+    This function calculate sum of all digits of a number.
+    :param num: str
+    :return: int
+    
+    >>> calculate_digits('15')
+    6
+    >>> print(calculate_digits('25'))
+    7
+    >>> calculate_digits('')
+    0
+    >>> calculate_digits('611091234512')
+    35
+    """
+
     sum_of_digits = 0
     for digit in num:
-        try:
-            sum_of_digits += int(digit)
-        except ValueError:
-            continue
+        sum_of_digits += int(digit)
     return sum_of_digits
 
 
-print(calculate_digits(15))
-print(calculate_digits(25))
-print(calculate_digits('611091234512'))
-print(calculate_digits('a5sd45g6'))
-print(calculate_digits('arey\nt@ry rtjh'))
-print(calculate_digits(''))
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
