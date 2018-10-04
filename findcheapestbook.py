1e5e62ef5bf9bbf7a908e8756c086a49b0f9f149
= <=>
2018-10-05 00:55:16 +0300

    find cheapest book

---
+++ findcheapestbook.py
@@ -0,0 +1,19 @@
+'''Find the cheapest book in list and return formatted string'''
+
+books = [
+    {
+        'name': 'Lord of the rings',
+        'price': 700
+    },
+    {
+        'name': 'Harry Potter',
+        'price': 1200
+    },
+    {
+        'name': 'Fluent Python',
+        'price': 555
+    }
+]
+
+cheapest_book = min(books, key=lambda x:x['price'])
+print("The cheapest book is ‘{0}’. It costs {1} grn." . format(cheapest_book['name'], cheapest_book['price']))
