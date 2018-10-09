58b123f6356b5088bcfbf98d8026fc7d33642597
Oleksiy Tiulpin <oleksiy@btech.com.ua>
2018-10-10 00:22:53 +0300

    Task3 done.

--- oleksijt/task3_calculate_squares_of_numers.py
+++ oleksijt/task3_calculate_squares_of_numers.py
@@ -1,30 +1,21 @@
-'''3. Calculate squares of numers
+""" This Script is Task#3 in Programming_education course.
 
-You have a list of numbers from 0 to 100000000000:
-0, 1, 2, …, 99999999999
+ The task is to calculate squares of numbers in range from
+ 0 to 100000000000.
+ The code should be able to execute on very simple computer
+ (without huge amount of virtual memory, CPU, etc.).
+ Code should works under both versions of Python (2.X, 3.X).
 
-Please create a new list with squares of numbers from previous list.
-Your new list should looks like:
-0*0, 1*1, 2*2, 3*3, 4*4, …,  99999999999*99999999999
-
-Plese print values from the second list.
-
-P.S. This task might look super easy, but there are a few cases. I guess, you’ll find them very quickly.
-Your code should be able to execute on very simple computer(without huge amount of virtual memory, CPU, etc.)
-Your code should works under both versions of Python (2.X, 3.X)
-'''
+ In result runs in Python 3.6 an also Python 2.7.15.
+ Python 2.7.15 runs with max_range up to 1000000.
+ """
 
 
-def quad_large_list(max_range):  # creates generator with max_range????
-    for num in range(0, max_range):
-        yield num*num  # returns quaded iterator
+def square_large_list(list_range):  # creates generator with max_range
+    for num in range(0, list_range):
+        yield num * num  # returns squared iterator
 
 
-max_range = 1000000
-for number in quad_large_list(max_range):
+max_range = 100000000000  # for Python 2.7.15 max_range limited to 1000000
+for number in square_large_list(max_range):
     print(number)
-
-
-# print(number for number in quad_large_list(1000))  # prints quaded iterators
-
-
