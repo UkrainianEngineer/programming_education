32105630c71ed6ad1d6940bb27a6a6198404dcd8
Sasha1152 <saxin@ukr.net>
2018-10-09 17:05:54 +0300

    adaptation code for Python 2

--- Oleksandr_Shmigelskiy/task03_squares_of_numbers.py
+++ Oleksandr_Shmigelskiy/task03_squares_of_numbers.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 start_time = datetime.now()
 
-big_list = range(10000000)
+big_list = range(1000000)
 big_list_square = (n**2 for n in big_list)
 
-print(*big_list_square)
+for n in big_list_square:
+    print(n)
 
 end_time = datetime.now()
 print('Duration: {}'.format(end_time - start_time))
