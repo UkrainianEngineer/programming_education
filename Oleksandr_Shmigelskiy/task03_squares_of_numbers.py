ba2d0e0c9637abb8d0a7b217e1aa8e6a9c9aead7
Sasha1152 <saxin@ukr.net>
2018-10-10 20:26:04 +0300

    adaptation code for both version Python

--- Oleksandr_Shmigelskiy/task03_squares_of_numbers.py
+++ Oleksandr_Shmigelskiy/task03_squares_of_numbers.py
@@ -1,11 +1,21 @@
+import sys
+
 from datetime import datetime
 start_time = datetime.now()
 
-big_list = range(1000000)
-big_list_square = (n**2 for n in big_list)
+big_number = 1000000
+big_list = []
 
+if sys.version[0] == '2':
+    big_list = xrange(big_number)
+elif sys.version[0] == '3':
+    big_list = range(big_number)
+
+
+big_list_square = (n**2 for n in big_list)
 for n in big_list_square:
     print(n)
+
 
 end_time = datetime.now()
 print('Duration: {}'.format(end_time - start_time))
