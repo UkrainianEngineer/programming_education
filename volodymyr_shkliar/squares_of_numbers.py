ebb6fff8ad6f795bfa81b06d2403f11a3fd440dc
Volodymyr Shkliar <volodyashklyar@gmail.com>
2018-10-10 23:28:17 +0300

    Add support for Python 2.X.

--- volodymyr_shkliar/squares_of_numbers.py
+++ volodymyr_shkliar/squares_of_numbers.py
@@ -1,9 +1,14 @@
 """
 Calculate squares of numbers.
 """
+import sys
 
+NUMBERS = 100000000000
 
-gen_exp = (x ** 2 for x in range(100000000000))
+if sys.version_info[0] == 2:
+    gen_exp = (x*x for x in xrange(NUMBERS))
+else:
+    gen_exp = (x*x for x in range(NUMBERS))
 
 
 if __name__ == '__main__':
