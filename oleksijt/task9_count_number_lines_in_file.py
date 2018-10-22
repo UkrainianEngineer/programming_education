2e03437fc915b2721d00ab0a94781ba9224e28b8
Oleksiy Tiulpin <oleksiy@btech.com.ua>
2018-10-22 23:26:21 +0300

    Task 9 completed.

--- oleksijt/task9_count_number_lines_in_file.py
+++ oleksijt/task9_count_number_lines_in_file.py
@@ -1,5 +1,10 @@
-def read_file_lines(file_name, nlines)
-    with open(file_name, 'r') as f:
-        file_content = f.readlines()
-        return len(file_content)
-print(read_file_lines(file_name, nlines))
+""" This script counts the number of lines in a text file. """
+
+
+def read_file_lines(file_name):
+    with open(file_name, 'r', encoding='utf-8') as f:
+        return len(f.readlines())
+
+
+print('The number of lines in file is {}.'.format(
+        read_file_lines('task7_demo_file.txt')))
