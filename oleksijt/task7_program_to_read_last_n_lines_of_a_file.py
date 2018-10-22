0ab6f5d1024d3efee61dac2f67021521646ea9d1
Oleksiy Tiulpin <oleksiy@btech.com.ua>
2018-10-22 17:07:36 +0300

    Task 7 completed

--- oleksijt/task7_program_to_read_last_n_lines_of_a_file.py
+++ oleksijt/task7_program_to_read_last_n_lines_of_a_file.py
@@ -1,5 +1,12 @@
-""" This code reads last n lines of a file. """
-def read_file_lines(file_name, nlines)
-    with open(file_name, 'r'):
+""" This script reads last n lines of a file. """
 
+
+def read_file_lines(file_name, num_lines):
+    with open(file_name, 'r', encoding='utf-8') as f:
+        file_lines = f.readlines()
+        return file_lines[len(file_lines) - num_lines:]
+
+
+for lines in read_file_lines('task7_demo_file.txt', 4):
+    print(lines.strip("\n"))
 
