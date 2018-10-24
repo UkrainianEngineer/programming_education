a77196d7eb252990b394d3d8fb7b4e8ff011b580
Oleksiy Tiulpin <oleksiy@btech.com.ua>
2018-10-25 00:05:28 +0300

    Task 14 done.

---
+++ oleksijt/oleksijt_task14_print_sentense_in_reverse_order.py
@@ -0,0 +1,15 @@
+""" This function prints a sentence in a reverse order.
+
+Input example:
+This is a test sentense
+
+Output example:
+esnetnes tset a si sihT
+"""
+
+
+def print_reverse(sentence):
+    print(sentence[::-1])
+
+
+print_reverse('This is a test sentense')
