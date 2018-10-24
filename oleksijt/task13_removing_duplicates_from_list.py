74a6bc3680a6efd2d64a5c18358ad80f496c239f
Oleksiy Tiulpin <oleksiy@btech.com.ua>
2018-10-24 18:59:40 +0300

    Task 13 completed.

--- oleksijt/task13_removing_duplicates_from_list.py
+++ oleksijt/task13_removing_duplicates_from_list.py
@@ -1,2 +1,12 @@
-"""13. With a given list `[12,24,35,24,88,120,155,88,120,155]`, write a program
-to print this list after removing all duplicate values with original order reserved. """
+""" Script returns list without duplicates in original order. """
+
+
+def remove_duplicates(my_list):
+    new_list = []
+    for element in my_list:
+        if element not in new_list:
+            new_list.append(element)
+    return new_list
+
+
+print(remove_duplicates([12, 24, 35, 24, 88, 120, 155, 88, 120, 155]))
