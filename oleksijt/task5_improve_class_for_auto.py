facedf2afdef37b16e8389a9edb03b2a17aa02ba
Oleksiy Tiulpin <oleksiy@btech.com.ua>
2018-10-26 22:26:06 +0300

    Comments fixed

--- oleksijt/task5_improve_class_for_auto.py
+++ oleksijt/task5_improve_class_for_auto.py
@@ -14,42 +14,31 @@ Methods:
  - get car max speed
 """
 
-import sys
-
 
 class Car:
     """Base class for autos."""
-    car_class_types = {
-        'Petrol': 'PetrolCar',
-        'Gas': 'GasCar',
-        'Electro': 'ElectroCar',
-        'Car': 'Car'
-    }
-
     def __init__(self, model, year, max_speed, car_type="Car"):
         self.car_type = car_type
         self.model = model
         self.year = year
         self.max_speed = max_speed
 
-    def find_new_car_class(car_type):
-        # Find class for new car type.
-        found_class = Car.car_class_types.get(car_type)
-        if found_class:
-            return found_class
-        else:
-            print('New car type not found. Base class set.')
-            return 'Car'
-
     def change_type(self, car_type):
         # Changes only car_type parameter without class changing.
         self.car_type = car_type
 
-    def set_car_type(self, car_type):
+    def set_car_type(self, new_type):
         # Changes class of object Car.
-        new_car_class = Car.find_new_car_class(car_type.title())
-        self.__class__ = getattr(sys.modules[__name__], new_car_class)
-        self.car_type = car_type
+        found_class = False
+        for car_class in Car.__subclasses__():
+            if car_class.car_type == new_type.title():
+                self.__class__ = car_class
+                self.car_type = new_type
+                found_class = True
+        if not found_class:
+            self.__class__ = Car
+            print('Class for new car type not found. Base class set.')
+
         return self
 
     def get_car_type(self):
@@ -67,23 +56,29 @@ class Car:
 
 class GasCar(Car):
     """Gas car class."""
+    car_type = 'Gas'
+
     def __init__(self):
         super().__init__()
-        self.car_type = 'Gas'
+        self.car_type = self.__class__.car_type
 
 
 class ElectroCar(Car):
     """Electro car class."""
+    car_type = 'Electro'
+
     def __init__(self):
         super().__init__()
-        self.car_type = 'Electro'
+        self.car_type = self.__class__.car_type
 
 
 class PetrolCar(Car):
     """Petrol car class."""
+    car_type = 'Petrol'
+
     def __init__(self):
         super().__init__()
-        self.car_type = 'Petrol'
+        self.car_type = self.__class__.car_type
 
 
 # Create new object 'Car'
@@ -99,6 +94,7 @@ car = Car('BMW', 2018, 300)
 print(car)
 
 # Change object class
+petrol_car = car.set_car_type('petroewhgtl')  # It returns Car instance.
 petrol_car = car.set_car_type('petrol')  # It returns `PetrolCar` instance.
 print(petrol_car)
 print(petrol_car.get_model())  # Returns `BMW`
