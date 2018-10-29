ec01968fe0ec3b0c86e1c206af5bacabe90eb7d4
Olya Petryshyn <olya.petryshyn@gmail.com>
2018-10-29 22:55:13 +0200

    now my function returns last n lines of the file.

--- olya_petryshyn/read_n_last_lines_from_file.py
+++ olya_petryshyn/read_n_last_lines_from_file.py
@@ -2,12 +2,14 @@
 Reading last n lines from the file.
 """
 def read_n_last_lines(file_name, n):
+    last_n_lines = ""
     with open(file_name) as file:
         text = file.readlines()
-        for line in (text[-n:]):
-            print(line, end='')
+        for line in text[-n:]:
+            last_n_lines += line
+        return last_n_lines
 
 
 file_name = "textfile.txt"
 n = 3
-read_n_last_lines(file_name, n)  # prints 3 last lines of the file.
+print(read_n_last_lines(file_name, n)) # prints 3 last lines of the file.
