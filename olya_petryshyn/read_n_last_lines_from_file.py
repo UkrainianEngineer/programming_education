60150e79b68897b1583b311746624c250037ac7d
Olya Petryshyn <olya.petryshyn@gmail.com>
2018-10-31 23:43:48 +0200

    simplified my fuction to return last n lines without additional variable.

--- olya_petryshyn/read_n_last_lines_from_file.py
+++ olya_petryshyn/read_n_last_lines_from_file.py
@@ -2,12 +2,9 @@
 Reading last n lines from the file.
 """
 def read_n_last_lines(file_name, n):
-    last_n_lines = ""
     with open(file_name) as file:
         text = file.readlines()
-        for line in text[-n:]:
-            last_n_lines += line
-        return last_n_lines
+        return "".join(text[-n:])
 
 
 file_name = "textfile.txt"
