e25f973443a5d5a6985ad30bed6f29fbf02565cf
Olya Petryshyn <olya.petryshyn@gmail.com>
2018-11-02 09:45:27 +0200

    Modified read_n_last_lines function.

--- olya_petryshyn/read_n_last_lines_from_file.py
+++ olya_petryshyn/read_n_last_lines_from_file.py
@@ -4,9 +4,10 @@ Reading last n lines from the file.
 def read_n_last_lines(file_name, n):
     with open(file_name) as file:
         text = file.readlines()
-        return "".join(text[-n:])
+        return text[-n:]
 
 
 file_name = "textfile.txt"
 n = 3
-print(read_n_last_lines(file_name, n)) # prints 3 last lines of the file.
+last_n_lines = "".join(read_n_last_lines(file_name, n))
+print(last_n_lines)  # prints 3 last lines of the file.
