4d2b9df14c3e42c7dbe18d8c56d13fa7503f2e29
Olya Petryshyn <olya.petryshyn@gmail.com>
2018-11-05 08:49:31 +0200

    Added docstring and simplified the function.

--- olya_petryshyn/task0013_list_with_unique_items.py
+++ olya_petryshyn/task0013_list_with_unique_items.py
@@ -1,9 +1,9 @@
+"""
+Getting rid of repeated items in a list and printing it reversed.
+"""
 list_of_ints = [12, 24, 35, 24, 88, 120, 155, 88, 120, 155]
 
 def unique_list_reversed(some_list):
-    new_set = set()
-    for item in some_list:
-        new_set.add(item)
-    return list(new_set)[::-1]
+    return list(set(some_list))[::-1]
 
 print(unique_list_reversed(list_of_ints))  # prints [155, 120, 24, 88, 12, 35]
