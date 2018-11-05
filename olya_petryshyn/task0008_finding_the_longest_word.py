19d611bb8a8f5d41c989172f5ee52aaed445971d
Olya Petryshyn <olya.petryshyn@gmail.com>
2018-11-05 12:13:24 +0200

    Modified the way to remove punctuation from string.

--- olya_petryshyn/task0008_finding_the_longest_word.py
+++ olya_petryshyn/task0008_finding_the_longest_word.py
@@ -4,12 +4,14 @@ Finding the longest word in the file.
 import string
 file_name = 'textfile.txt'
 
-def longest_word(file_name):
+def the_longest_word(file_name):
     with open(file_name, 'r') as file:
         string_text = file.read().replace('\n', ' ')
+    for item in string.punctuation:
+        string_text = string_text.replace(item, '')
     list_text = string_text.split()
-    list_text = [''.join(character for character in word if character not in string.punctuation) for word in list_text]
     list_text.sort(key=lambda x: len(x))
     return list_text[-1]
 
-print(longest_word(file_name))
+
+print(the_longest_word(file_name))
