8fa1fef0a740f1b401e6a44ce3b7d3a2cdaba879
Olya Petryshyn <olya.petryshyn@gmail.com>
2018-11-07 21:03:05 +0200

    Simplified my func to find the longest word.

--- olya_petryshyn/task0008_finding_the_longest_word.py
+++ olya_petryshyn/task0008_finding_the_longest_word.py
@@ -4,14 +4,13 @@ Finding the longest word in the file.
 import string
 file_name = 'textfile.txt'
 
+
 def the_longest_word(file_name):
     with open(file_name, 'r') as file:
         string_text = file.read().replace('\n', ' ')
-    for item in string.punctuation:
-        string_text = string_text.replace(item, '')
+    string_text = string_text.translate(None, string.punctuation)
     list_text = string_text.split()
-    list_text.sort(key=lambda x: len(x))
-    return list_text[-1]
+    return max(list_text, key=len)
 
 
 print(the_longest_word(file_name))
