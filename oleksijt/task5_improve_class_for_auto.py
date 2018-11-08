d9457161caedd1de20298eaf5b30013ca40c53ad
Oleksiy Tiulpin <oleksiy@btech.com.ua>
2018-11-08 22:36:51 +0200

    Done without magic methods.

--- oleksijt/task5_improve_class_for_auto.py
+++ oleksijt/task5_improve_class_for_auto.py
@@ -14,12 +14,14 @@ Methods:
  - get car max speed
 """
 
-import sys, inspect
+import inspect
+import sys
 
 
 class Car:
     """Base class for autos."""
     car_type = 'Car'
+
     def __init__(self, model, year, max_speed, car_type="Car"):
         self.car_type = car_type
         self.model = model
@@ -31,18 +33,16 @@ class Car:
         self.car_type = car_type
 
     def set_car_type(self, new_type):
-        # Changes class of object Car.
+        # Creates new object from self with the same parameters.
         found_class = False
         for key, data in inspect.getmembers(sys.modules[__name__], inspect.isclass):
             if data.car_type == new_type.title():
-                setattr(self, '__class__', getattr(sys.modules[__name__], data.__name__, Car))
-                self.car_type = new_type
+                new_object = data(self.model, self.year, self.max_speed)
                 found_class = True
         if not found_class:
-            self.__class__ = Car
+            new_object = Car(self.model, self.year, self.max_speed)
             print('Class for new car type not found. Base class set.')
-
-        return self
+        return new_object
 
     def get_car_type(self):
         return self.car_type.title()
@@ -61,53 +61,50 @@ class GasCar(Car):
     """Gas car class."""
     car_type = 'Gas'
 
-    def __init__(self):
-        super().__init__()
-        self.car_type = self.__class__.car_type
+    def __init__(self, model, year, max_speed):
+        Car.__init__(self, model, year, max_speed, self.car_type)
 
 
 class ElectroCar(Car):
     """Electro car class."""
     car_type = 'Electro'
 
-    def __init__(self):
-        super().__init__()
-        self.car_type = self.__class__.car_type
+    def __init__(self, model, year, max_speed):
+        Car.__init__(self, model, year, max_speed, self.car_type)
 
 
 class PetrolCar(Car):
     """Petrol car class."""
     car_type = 'Petrol'
 
-    def __init__(self):
-        super().__init__()
-        self.car_type = self.__class__.car_type
+    def __init__(self, model, year, max_speed):
+        Car.__init__(self, model, year, max_speed, self.car_type)
 
 
 if __name__ == '__main__':
-    # Create new object 'Car'
+    # Create new object 'Car'.
     car1 = Car('BMW', 2018, 280, 'gas')
     print(car1.get_car_type())
 
-    # Change type of object 'Car'
+    # Change type of object 'Car'.
     car1.change_type('petrol')
     print(car1.get_car_type())
 
-    # Create  new object 'Car'
+    # Create  new object 'Car'.
     car = Car('BMW', 2018, 300)
-    print(car)
+    print(car, car.__dict__)
 
-    # Change object class
+    # Create similar object class in new class
     petrol_car = car.set_car_type('petroewhgtl')  # It returns Car instance.
-    print(petrol_car)
+    print(petrol_car, petrol_car.__dict__)
     petrol_car = car.set_car_type('petrol')  # It returns `PetrolCar` instance.
-    print(petrol_car)
+    print(petrol_car, petrol_car.__dict__)
     print(petrol_car.get_model())  # Returns `BMW`
     print(petrol_car.get_year())  # Returns 2018
     print(petrol_car.get_max_speed())  # Returns 300
     print(petrol_car.get_car_type())  # Returns Petrol
     gas_car = car.set_car_type('gas')  # It returns `GasCar` instance
-    print(gas_car)
-    print(gas_car.get_car_type())  # Returns Petrol
+    print(gas_car, gas_car.__dict__)
+    print(gas_car.get_car_type())  # Returns Gas
     print(petrol_car)
     print(car)
