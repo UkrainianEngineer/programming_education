efa1c4cce02e440a352ddb513eae2d427223461d
Oleksiy Tiulpin <oleksiy@btech.com.ua>
2018-11-10 11:39:09 +0200

    Code made more obvious.

--- oleksijt/task7_program_to_read_last_n_lines_of_a_file.py
+++ oleksijt/task7_program_to_read_last_n_lines_of_a_file.py
@@ -3,10 +3,8 @@
 
 def read_file_lines(file_name, num_lines):
     with open(file_name, 'r', encoding='utf-8') as f:
-        file_lines = f.readlines()
-        return file_lines[len(file_lines) - num_lines:]
+        return f.readlines()[- num_lines:]
 
 
-for lines in read_file_lines('task7_demo_file.txt', 4):
-    print(lines.strip("\n"))
+print(read_file_lines('task7_demo_file.txt', 4))
 
