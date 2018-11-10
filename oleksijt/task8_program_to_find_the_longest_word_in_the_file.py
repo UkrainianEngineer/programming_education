d64703cd658e5463b1c0df1c48daa5b5fad0c1a1
Oleksiy Tiulpin <oleksiy@btech.com.ua>
2018-11-10 19:25:18 +0200

    Python2 supporting added.

--- oleksijt/task8_program_to_find_the_longest_word_in_the_file.py
+++ oleksijt/task8_program_to_find_the_longest_word_in_the_file.py
@@ -1,14 +1,26 @@
-""" This script finds the longest word in the file. """
+""" This script finds the longest word in the file.
+Works only with utf-8 files in 3.x and 2.x versions of Python.
+Tested on Python 2.7, 3.6 and 3.7.
+"""
+
+import io
+import sys
 
 
 def find_longest_word(file_name):
-    with open(file_name, 'r', encoding='utf-8') as f:
+    with io.open(file_name, 'r', encoding='utf-8') as f:
 
-        # Remove punctuation marks.
-        file_content = f.read().translate(str.maketrans('', '', '[],.;:!?'))
+        file_content = f.read()
+
+        # Remove punctuation marks in different Python versions.
+        wrong_characters = '[],.;:!?'
+        if int(sys.version[0]) >= 3:
+            words = file_content.translate(str.maketrans('', '', wrong_characters))
+        else:
+            words = file_content.encode('utf-8').translate(None, wrong_characters)
 
         # Split file content into the words.
-        words = file_content.split()
+        words = words.split()
 
         # Find longest word.
         longest_word = max(words, key=len)
