084423637d7651a5f9d7a14ab8bbbb7f3933f1a6
Oleksiy Tiulpin <oleksiy@btech.com.ua>
2018-11-28 22:30:56 +0200

    Decorator improved.

--- oleksijt/users_without_class.py
+++ oleksijt/users_without_class.py
@@ -8,32 +8,36 @@
      decorated by access_control function.
 """
 
-admin_name = 'Oleksiy'
+admins_list = ['pivanchy', 'oleksijt']
+moderators_list = ['Yuriy', 'oleksijt']
 
 
-def access_control(func):
-    # Decorator function which controls user access to functions
-    def wrapper(*args, **kwargs):
-        if args[0] != admin_name:
-            raise PermissionError('Permission denied.')
-        return func(*args, **kwargs)
-    return wrapper
+# Decorator function which controls user access to functions
+def allowed_users(user_list):
+    def access_control(func):
+        def wrapper(*args, **kwargs):
+            if args[0] not in user_list:
+                raise PermissionError('Permission denied.')
+            return func(*args, **kwargs)
+        return wrapper
+    return access_control
 
 
-@access_control
+@allowed_users(admins_list)
 def get_user_info(name):
     # Returns  user's name.
     return name
 
 
-@access_control
+@allowed_users(moderators_list)
 def check_perms(name):
     # Function checks permission for user.
     return 'Allowed access'
 
+
 if __name__ == '__main__':
-    print(get_user_info('Oleksiy'))  # Prints `Oleksiy`
+    print(get_user_info('oleksijt'))  # Prints `Oleksiy`
     print(get_user_info('Yuriy'))  # Prints `Permission denied.`
 
-    print(check_perms('Oleksiy'))  # Prints `Allowed access`
+    print(check_perms('pivanchy'))  # Prints `Allowed access`
     print(check_perms('Yuriy'))  # Prints `Permission denied.`
